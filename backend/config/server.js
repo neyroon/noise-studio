93b3a3d6afd6d1d9ecad50e3840ea17c2ac9e36a
neyroon <moralis.facusdoom@yandex.ru>
2021-07-20 18:15:46 +0400

    test

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c4aa0d12510c06a35540a3d3f89eae0a'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
