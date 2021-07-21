0a7772c44f91b948992ff4a2428d0b4c97f08601
neyroon <moralis.facusdoom@yandex.ru>
2021-07-21 23:59:22 +0400

    set up next

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -6,19 +6,9 @@ const Header = () => {
     <div className="flex justify-between mt-4">
       <Link href="/">
         <a>
-          <Image
-            src="/strapi.png"
-            alt="home"
-            className="logo"
-            height="150"
-            width="150"
-          />
+          <Image src="/strapi.png" alt="home" className="logo" />
         </a>
       </Link>
-      <button className="snipcart-checkout flex items-center">
-        <img src="/cart.svg" alt="Cart" />
-        <span className="snipcart-total-price ml-3 font-semibold text-sm text-indigo-500"></span>
-      </button>
     </div>
   );
 };
---
+++ frontend/pages/404.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function Custom404() {
+  return (
+    <>
+      <Header></Header>
+      <h1 className="text-9xl">404 - Page Not Found</h1>
+    </>
+  );
+}
