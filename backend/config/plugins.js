aca57d5ba8c3ae31a2575254701712f396ea04f1
neyroon <moralis.facusdoom@yandex.ru>
2021-07-22 11:26:48 +0400

    fdffd

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -3,7 +3,11 @@ module.exports = {
   graphql: {
     endpoint: "/graphql",
     shadowCRUD: true,
+    playgroundAlways: false,
     depthLimit: 7,
     amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
   },
 };
