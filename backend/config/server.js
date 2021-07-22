418c04adae4c2d7de9ed81b1ba71af648d8e8e7a
neyroon <moralis.facusdoom@yandex.ru>
2021-07-22 22:16:48 +0400

    FDFD

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("NEXT_PUBLIC_STRAPI_API_URL", ""),
+  url: env("API_URL", ""),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
