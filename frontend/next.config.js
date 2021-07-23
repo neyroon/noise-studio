e504a20eb5d4d4d1f4e5654cf27f9e52bbca353d
neyroon <moralis.facusdoom@yandex.ru>
2021-07-24 01:01:45 +0400

    add domains field to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  images: {
+    domains: ["noise-studio.ru", "localhost"],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
