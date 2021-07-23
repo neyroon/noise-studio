e5e76cb9852f73d4e7f48ec43eb85390fdf4fb4a
neyroon <moralis.facusdoom@yandex.ru>
2021-07-24 01:05:12 +0400

    add wrapper for rendering only on the client side delete spinner and add dynamically loaded Cart component

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,20 @@
 import App from "next/app";
 import Router from "next/router";
+import NProgress from "nprogress";
 import { ApolloProvider } from "@apollo/client";
 import Layout from "../components/Layout";
 import { getCategoriesQuery } from "../utils/api";
 import { initializeApollo, useApollo } from "../utils/apolloClient";
 import "tailwindcss/tailwind.css";
 import "../styles/index.scss";
-import NProgress from "nprogress";
 import "nprogress/nprogress.css";
+import dynamic from "next/dynamic";
+
+const CartContextProvider = dynamic(() => import("../context/CartContext"), {
+  ssr: false,
+});
+
+NProgress.configure({ showSpinner: false });
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -17,26 +24,21 @@ const MyApp = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
-      <Layout categories={pageProps.categories}>
-        <Component {...pageProps} />
-      </Layout>
+      <CartContextProvider>
+        <Layout categories={pageProps.categories}>
+          <Component {...pageProps} />
+        </Layout>
+      </CartContextProvider>
     </ApolloProvider>
   );
 };
 
-// getInitialProps disables automatic static optimization for pages that don't
-// have getStaticProps. So [[...slug]] pages still get SSG.
-// Hopefully we can replace this with getStaticProps once this issue is fixed:
-// https://github.com/vercel/next.js/discussions/10949
 MyApp.getInitialProps = async (ctx) => {
-  // Calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(ctx);
   const client = initializeApollo();
-  // Fetch global site settings from Strapi
   const { loading, data, error } = await client.query({
     query: getCategoriesQuery,
   });
-  // Pass the data to our page via props
   return { ...appProps, pageProps: { categories: data.categories } };
 };
 
